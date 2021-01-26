280ec44126485b9c39900da360b6907ca433bf7b
Aditi-Trishna <77819803+Aditi-Trishna@users.noreply.github.com>
2021-01-26 21:04:04 +0530

    Add files via upload
    Assignment 4 submitted.

---
+++ Day5Q2.js
@@ -0,0 +1,10 @@
+
+function Add() 
+{
+    var num1 = document.getElementsByTagName("input");
+    var num2 = document.getElementsByTagName("input");
+    var sum = num1+num2;
+    console.log(sum);
+}
+
+Add();
